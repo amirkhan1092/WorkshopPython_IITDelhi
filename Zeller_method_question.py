a3b55b86ec15a471a041798b533a2a69f42db924
amirkhan1092 <amirkhan1092@gmail.com>
2019-09-04 23:18:49 +0530

    Zeller method problem

---
+++ Zeller_method_question.py
@@ -0,0 +1,41 @@
+# Python program to calculate Day of the week from a date input by the user
+date = eval(input("Please enter the date: ex - 22, 01, 19 etc\n"))
+month = eval(input("Please enter the month number: 1 for January, 2 for February, 3 for March and so on...\n"))
+year = eval(input("Please enter the year: ex - 2019, 2001 etc\n"))
+
+dateString = str(date) + "/" + str(month) + "/" + str(year)
+print("The date you have entered is", dateString)
+
+m = month
+if month == 1:
+    m = 13
+    year -= 1
+elif month == 2:
+    m = 14
+    year -= 1
+# D is the first two digits of the
+
+q = date
+
+k = year % 100
+j = year // 100
+h = q + 13 * (m + 1) // 5 + k + k // 4 + j // 4 + 5 * j
+res = h % 7
+
+dayString = ""
+if res == 0:
+    dayString = "Saturday"
+elif res == 1:
+    dayString = "Sunday"
+elif res == 3:
+    dayString = "Monday"
+elif res == 4:
+    dayString = "Tuesday"
+elif res == 5:
+    dayString = "Wednesday"
+elif res == 6:
+    dayString = "Thursday"
+elif res == 7:
+    dayString = "Friday"
+
+print(dateString, "was a", dayString)
