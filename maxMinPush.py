8cae844fef9b7069e1330337fb1b426f605f5fce
amirkhan1092 <amirkhan1092@gmail.com>
2019-10-16 23:46:23 +0530

    min max in a list

---
+++ maxMinPush.py
@@ -0,0 +1,14 @@
+def minMax(operations, x):
+    elements = []
+    ans = []
+
+    for k, i in enumerate(operations):
+        if i == 'push':
+            elements.append(x[k])
+        else:
+            elements.remove(x[k])
+        elements.sort()
+        ans.append(elements[0]*elements[-1])
+    return ans
+
+
